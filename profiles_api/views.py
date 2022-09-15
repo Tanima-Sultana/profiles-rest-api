13ee3c5b11923d9a2487c56d29dfe3caa18c44d7
Tanima <tanimasultana.du@gmail.com>
2022-09-15 12:49:40 +0600

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
@@ -105,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateProfilePermission,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
